1203e645c6a35497bc9b7aab89f73b06248e017d
Torsten Dahms <torsten.dahms@cern.ch>
2014-02-10 10:52:09 +0100

    new MC files from Mihee with bugfix on Gen information
    Signed-off-by: Torsten Dahms <torsten.dahms@cern.ch>

--- lJpsi_cut_tuning/set_weights.C
+++ lJpsi_cut_tuning/set_weights.C
@@ -1,53 +1,53 @@
 void set_weights()
 {
   // prompt
-  TFile *f0 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt03_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f0 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt03_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(1.0/117512.0);
   myTree->AutoSave();
   f0->Close();
-  TFile *f1 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt36_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f1 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt36_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(0.96075/124802.0);
   myTree->AutoSave();
   f1->Close();
-  TFile *f2 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt69_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f2 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt69_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(0.2136/172176.0);
   myTree->AutoSave();
   f2->Close();
-  TFile *f3 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt912_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f3 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt912_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(0.04385/167336.0);
   myTree->AutoSave();
   f3->Close();
-  TFile *f4 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt1215_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f4 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt1215_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(0.0112585/143944.0);
   myTree->AutoSave();
   f4->Close();
-  TFile *f5 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt1530_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f5 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt1530_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(0.00579/166960.0);
   myTree->AutoSave();
   f5->Close();
   /*
   // non-prompt
-  TFile *f0 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt03_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f0 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt03_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(1.0/171458.0);
   myTree->AutoSave();
   f0->Close();
-  TFile *f1 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt36_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f1 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt36_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(2.25*0.623/172208.0);
   myTree->AutoSave();
   f1->Close();
-  TFile *f2 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt69_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f2 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt69_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(2.55*0.245/171178.0);
   myTree->AutoSave();
   f2->Close();
-  TFile *f3 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt912_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f3 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt912_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(2.45*0.080/172208.0);
   myTree->AutoSave();
   f3->Close();
-  TFile *f4 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt1215_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f4 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt1215_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(2.0*0.0299/170670.0);
   myTree->AutoSave();
   f4->Close();
-  TFile *f5 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt1530_Histos_cmssw445p1_RegIt.root","UPDATE");
+  TFile *f5 = new TFile("/data/CMS/MC/PbPb/bJpsiMuMu_JpsiPt1530_Histos_cmssw445p5_RegIt.root","UPDATE");
   myTree->SetWeight(3.35*0.0109/169302.0);
   myTree->AutoSave();
   f5->Close();
--- plotting/plot_pt.C
+++ plotting/plot_pt.C
@@ -20,40 +20,45 @@ void plot_pt()
   TCut genCuts = "abs(Gen_QQ_4mom.Rapidity())<2.4";
   TCut muPlusAcc = "abs(Gen_QQ_mupl_4mom.Rapidity()<2.4)&&Gen_QQ_mupl_4mom.P()>2.5";
   TCut muMinusAcc = "abs(Gen_QQ_mumi_4mom.Rapidity()<2.4)&&Gen_QQ_mumi_4mom.P()>2.5";
-  TFile *f0 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt03_Histos_cmssw445p1_RegIt.root","READ");
+  TFile *f0 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt03_Histos_cmssw445p5_RegIt.root","READ");
   myTree->Draw("Gen_QQ_4mom.Pt()>>h0(500,0,50)",genCuts&&muPlusAcc&&muMinusAcc,"e");
   h0->Scale(1.0/117512.0);
   h0->SetLineColor(2);
   TH1F *hpt = (TH1F*)h0->Clone("hpt");
   hpt->SetDirectory(0);
 
-  TFile *f1 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt36_Histos_cmssw445p1_RegIt.root","READ");
+  TFile *f1 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt36_Histos_cmssw445p5_RegIt.root","READ");
   myTree->Draw("Gen_QQ_4mom.Pt()>>h1(500,0,50)",genCuts&&muPlusAcc&&muMinusAcc,"e");
-  h1->Scale(0.915/124802.0);
+  h1->Scale(0.96075/124802.0);
+  //  h1->Scale(0.915/124802.0);
   h1->SetLineColor(4);
   hpt->Add(h1);
 
-  TFile *f2 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt69_Histos_cmssw445p1_RegIt.root","READ");
+  TFile *f2 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt69_Histos_cmssw445p5_RegIt.root","READ");
   myTree->Draw("Gen_QQ_4mom.Pt()>>h2(500,0,50)",genCuts&&muPlusAcc&&muMinusAcc,"e");
-  h2->Scale(0.178/172176.0);
+  h2->Scale(0.2136/172176.0);
+  //  h2->Scale(0.178/172176.0);
   h2->SetLineColor(kGreen+2);
   hpt->Add(h2);
 
-  TFile *f3 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt912_Histos_cmssw445p1_RegIt.root","READ");
+  TFile *f3 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt912_Histos_cmssw445p5_RegIt.root","READ");
   myTree->Draw("Gen_QQ_4mom.Pt()>>h3(500,0,50)",genCuts&&muPlusAcc&&muMinusAcc,"e");
-  h3->Scale(0.0379/167336.0);
+  h3->Scale(0.04385/167336.0);
+  //  h3->Scale(0.0379/167336.0);
   h3->SetLineColor(kOrange+2);
   hpt->Add(h3);
 
-  TFile *f4 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt1215_Histos_cmssw445p1_RegIt.root","READ");
+  TFile *f4 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt1215_Histos_cmssw445p5_RegIt.root","READ");
   myTree->Draw("Gen_QQ_4mom.Pt()>>h4(500,0,50)",genCuts&&muPlusAcc&&muMinusAcc,"e");
-  h4->Scale(0.00979/143944.0);
+  h4->Scale(0.0112585/143944.0);
+  //  h4->Scale(0.00979/143944.0);
   h4->SetLineColor(kMagenta+1);
   hpt->Add(h4);
 
-  TFile *f5 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt1530_Histos_cmssw445p1_RegIt.root","READ");
+  TFile *f5 = new TFile("/data/CMS/MC/PbPb/jpsiMuMu_JpsiPt1530_Histos_cmssw445p5_RegIt.root","READ");
   myTree->Draw("Gen_QQ_4mom.Pt()>>h5(500,0,50)",genCuts&&muPlusAcc&&muMinusAcc,"e");
   h5->Scale(0.00579/166960.0);
+  //  h5->Scale(0.00579/166960.0);
   h5->SetLineColor(kCyan-2);
   hpt->Add(h5);
 
