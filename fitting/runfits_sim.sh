b78d2e0b7a0a069e9fb486abbfdc1f96cf60f091
Torsten Dahms <torsten.dahms@cern.ch>
2014-03-03 10:52:16 +0100

    Sim fits
    Signed-off-by: Torsten Dahms <torsten.dahms@cern.ch>

--- fitting/runfits_sim.sh
+++ fitting/runfits_sim.sh
@@ -2,7 +2,7 @@
 TODAY=`date +%Y%m%d`
 echo "Today is ${TODAY}"
 
-SUFFIX="DblMu0"
+SUFFIX="SimFits_M2242_FixCBtail_NoSumw2_DblMu0_NoMinos_finalCBtailFits_fixWideFactorPbPbMC_corrPsi2SError"
 
 DIRECTORY="${TODAY}_${SUFFIX}"
 
@@ -15,38 +15,86 @@ else
     echo "Continuing anyways, will overwrite files!"
 fi
 
-./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-100.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 6.5-30.0 -y 0.0-2.4 -t 0-100 -x 4
-./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-100.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 6.5-30.0 -y 0.0-2.4 -t 0-100 -x 4
-./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-100.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 6.5-30.0 -y 1.6-2.4 -t 0-100 -x 4
-./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-100.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 6.5-30.0 -y 1.6-2.4 -t 0-100 -x 4
-./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-100.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 3.0-30.0 -y 1.6-2.4 -t 0-100 -x 4
-./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-100.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 3.0-30.0 -y 1.6-2.4 -t 0-100 -x 4
-./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-100.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 3.0-6.5 -y 1.6-2.4 -t 0-100 -x 4
-./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-100.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 3.0-6.5 -y 1.6-2.4 -t 0-100 -x 4
+runSyst=1
+ptbins=(6.5-30.0 3.0-30.0)
+rapbins=(0.0-2.4 0.0-1.6 1.6-2.4)
+centbins=(0-100 0-20 20-40 40-100)
 
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-20.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 6.5-30.0 -y 0.0-2.4 -t 0-20 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-20.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 6.5-30.0 -y 0.0-2.4 -t 0-20 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-20.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 6.5-30.0 -y 1.6-2.4 -t 0-20 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-20.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 6.5-30.0 -y 1.6-2.4 -t 0-20 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-20.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 3.0-30.0 -y 1.6-2.4 -t 0-20 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-20.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 3.0-30.0 -y 1.6-2.4 -t 0-20 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-20.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 3.0-6.5 -y 1.6-2.4 -t 0-20 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent0-20.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 3.0-6.5 -y 1.6-2.4 -t 0-20 -x 4
-#
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent20-40.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 6.5-30.0 -y 0.0-2.4 -t 20-40 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent20-40.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 6.5-30.0 -y 0.0-2.4 -t 20-40 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent20-40.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 6.5-30.0 -y 1.6-2.4 -t 20-40 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent20-40.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 6.5-30.0 -y 1.6-2.4 -t 20-40 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent20-40.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 3.0-30.0 -y 1.6-2.4 -t 20-40 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent20-40.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 3.0-30.0 -y 1.6-2.4 -t 20-40 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent20-40.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 3.0-6.5 -y 1.6-2.4 -t 20-40 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent20-40.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 3.0-6.5 -y 1.6-2.4 -t 20-40 -x 4
-#
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent40-100.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 6.5-30.0 -y 0.0-2.4 -t 40-100 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent40-100.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 6.5-30.0 -y 0.0-2.4 -t 40-100 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent40-100.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 6.5-30.0 -y 1.6-2.4 -t 40-100 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent40-100.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 6.5-30.0 -y 1.6-2.4 -t 40-100 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent40-100.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 3.0-30.0 -y 1.6-2.4 -t 40-100 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent40-100.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 3.0-30.0 -y 1.6-2.4 -t 40-100 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent40-100.root ../root_files/ppData2011_cent0-100.root -v signalCB2WN signalCB2WNP expFunct_HI expFunct_pp  -d simLogCB  -u -p 3.0-6.5 -y 1.6-2.4 -t 40-100 -x 4
-#./Fit1DDataPbPbSim -f ../root_files/Data2011_cent40-100.root ../root_files/ppData2011_cent0-100.root -v sigCB2WNG1 sigCB2WNG1P expFunct_HI expFunct_pp -d simLogCBG -u -p 3.0-6.5 -y 1.6-2.4 -t 40-100 -x 4
+for pt in "${ptbins[@]}";
+do
+    for rap in "${rapbins[@]}";
+    do
+	for cent in "${centbins[@]}";
+	do
+	    if [[ ${cent} != "0-100" && ${pt} != "3.0-30.0" && ${rap} == "1.6-2.4" ]];
+	    then
+		echo "skipping " ${rap} " " ${pt} " " ${cent};
+		continue;
+	    fi;
+	    if [[ ${cent} == "0-100" && ${pt} == "3.0-6.5" && ${rap} == "1.6-2.4" ]];
+	    then
+		bkg="pol2_HI pol1";
+	    elif [[ ${cent} == "0-100" && ${pt} == "6.5-30.0" && ${rap} == "1.6-2.4" ]];
+	    then
+		bkg="pol3_HI pol5";
+	    elif [[ ${pt} == "6.5-30.0" && ${rap} == "0.0-2.4" ]]
+	    then
+		if [[ ${cent} == "0-100" ]]
+		then
+		    bkg="pol1_HI pol5";
+		elif [[ ${cent} == "0-20" ]]
+		then
+		    bkg="pol1_HI020 pol5";
+		elif [[ ${cent} == "20-40" ]]
+		then
+		    bkg="pol2_HI pol5";
+		elif [[ ${cent} == "40-100" ]]
+		then
+		    bkg="pol1_HI pol5";
+		fi;
+	    elif [[ ${pt} == "6.5-30.0" && ${rap} == "0.0-1.6" ]]
+	    then
+		if [[ ${cent} == "0-100" ]]
+		then
+		    bkg="pol1_HI pol3";
+		elif [[ ${cent} == "0-20" ]]
+		then
+		    bkg="pol1_HI020 pol3";
+		elif [[ ${cent} == "20-40" ]]
+		then
+		    bkg="pol1_HI pol3";
+		elif [[ ${cent} == "40-100" ]]
+		then
+		    bkg="pol1_HI pol3";
+		fi;
+	    elif [[ ${pt} == "3.0-30.0" && ${rap} == "1.6-2.4" ]]
+	    then
+		if [[ ${cent} == "0-100" ]]
+		then
+		    bkg="pol1_HI pol5";
+		elif [[ ${cent} == "0-20" ]]
+		then
+		    bkg="pol3_HI020 pol5";
+		elif [[ ${cent} == "20-40" ]]
+		then
+		    bkg="pol2_HI pol5";
+		elif [[ ${cent} == "40-100" ]]
+		then
+		    bkg="pol1_HI pol5";
+		fi;
+	    else
+		echo "skipping " ${rap} " " ${pt} " " ${cent};
+		continue;
+	    fi;
+	    echo "fitting " ${rap} " " ${pt} " " ${cent} " " ${bkg};
+	    ./Fit1DDataPbPbSim -f ../root_files/PbPbData2011_DblMu0_cent${cent}_M22-42.root ../root_files/ppData2013_DblMu0_cent0-100_M22-42.root -v signalCB1 signalCB1P ${bkg} -d ${DIRECTORY}/fracLogCB -p ${pt} -y ${rap} -t ${cent} -l 1 -b 1 &> ${DIRECTORY}/sim_fitCB_rap${rap}_pt${pt}_cent${cent}.log
+	    ./Fit1DDataPbPbSim -f ../root_files/PbPbData2011_DblMu0_cent${cent}_M22-42.root ../root_files/ppData2013_DblMu0_cent0-100_M22-42.root -v sigCB1G2 sigCB1G2P ${bkg} -d ${DIRECTORY}/fracLogCBG -p ${pt} -y ${rap} -t ${cent} -l 1 -b 1 &> ${DIRECTORY}/sim_fitCBG_rap${rap}_pt${pt}_cent${cent}.log
+	done;
+    done;
+done;
+
+logfiles=(${DIRECTORY}/sim_fitCB*.log)
+if [[ -e "${logfiles[0]}" ]];
+then
+    gzip -f ${DIRECTORY}/sim_fitCB*.log;
+fi;
